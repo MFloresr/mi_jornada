705a98a846f33fa6534433330a0067242d37198b
Mario Flores <mrsitofull@gmail.com>
2026-06-19 08:20:45 +0200

    incluimos templates y cambio urls

--- backend/mi_jornada/urls.py
+++ backend/mi_jornada/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path('api/', include('time_tracker.urls')),  # API para el frontend
 ]
